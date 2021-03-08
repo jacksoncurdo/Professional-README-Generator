85a0bcd86ee8490b95ea2ecafeaa7f33a2620467
Jackson Curdo <75642820+jacksoncurdo@users.noreply.github.com>
2021-03-07 20:08:36 -0700

    filled out Markdown.js file

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,19 +1,31 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
-
+function generateMarkdown(userInput) {
+  return `# ${userInput.title}
+  ![badge](https://img.shields.io/badge/license-${userInput.license}-blue)
+  ## Table of contents
+  - [Description](#description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributors](#contributors)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  ## Description
+  ${userInput.description}
+  ## Installation
+  ${userInput.installation}
+  ## Usage
+  ${userInput.usage}
+  ## License
+  ![badge](https://img.shields.io/badge/license-${userInput.license}-blue)
+  The license covering this application is the ${userInput.license} license.
+  ## Contributors
+  ${userInput.contributors}
+  ## Tests
+  ${userInput.tests}
+  ## Questions
+  ${userInput.questions}
+  You can find me on GitHub at [${userInput.github}](https://github.com/${userInput.github})
+  Email me if you run into any questions at ${userInput.email}
 `;
 }
 
