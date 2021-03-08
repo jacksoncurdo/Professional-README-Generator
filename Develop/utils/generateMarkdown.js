9762225cbb1dc4c97a48c8bd6a0ed3993d003f4a
Jackson Curdo <75642820+jacksoncurdo@users.noreply.github.com>
2021-03-07 20:18:13 -0700

    made a change to the markdown.js

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,8 @@
+// TODO: Create a function to generate markdown for README
 function generateMarkdown(userInput) {
   return `# ${userInput.title}
   ![badge](https://img.shields.io/badge/license-${userInput.license}-blue)
+
   ## Table of contents
   - [Description](#description)
   - [Installation](#installation)
@@ -9,19 +11,26 @@ function generateMarkdown(userInput) {
   - [Contributors](#contributors)
   - [Tests](#tests)
   - [Questions](#questions)
+
   ## Description
   ${userInput.description}
+
   ## Installation
   ${userInput.installation}
+
   ## Usage
   ${userInput.usage}
+
   ## License
   ![badge](https://img.shields.io/badge/license-${userInput.license}-blue)
   The license covering this application is the ${userInput.license} license.
+
   ## Contributors
   ${userInput.contributors}
+
   ## Tests
   ${userInput.tests}
+
   ## Questions
   ${userInput.questions}
   You can find me on GitHub at [${userInput.github}](https://github.com/${userInput.github})
